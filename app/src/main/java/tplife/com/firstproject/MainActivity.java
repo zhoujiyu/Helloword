18f3564bdbfda740f5bb02f306b17b7e35917585
“Test <oss.lzu.edu.cn@gmail.com>
2016-11-11 16:25:37 +0800

    add a method；

--- app/src/main/java/tplife/com/firstproject/MainActivity.java
+++ app/src/main/java/tplife/com/firstproject/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
     }
+
+    private void  test(){
+        System.out.println("this is my first test");
+    }
+
+
+
 }
